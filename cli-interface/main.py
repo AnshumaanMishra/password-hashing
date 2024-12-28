50b6ab31079e701984b01be359429c1caf89bba9
AnshumaanMishra <anshumaanmishra.24@kgpian.iitkgp.ac.in>
2024-12-28 19:07:24 +0530

    Bug Fix

--- cli-interface/main.py
+++ cli-interface/main.py
@@ -9,3 +9,5 @@ while (choice == 1 or choice == 2):
         login.login()
     elif(choice == 2):
         register.register()
+    print("Enter Your Choice:\n\t1. Login\n\t2. Register\n\t0. Quit")
+    choice = int(input())
